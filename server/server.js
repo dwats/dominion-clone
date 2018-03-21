92feb7c337e976796daacf0b2c012ee066cdb350
Eric Julius <ejulius86@gmail.com>
2018-03-21 12:33:19 -0500

    Imported router and setup basic app.use
    app.use - boiler plate public folder and basic routes added.

--- server/server.js
+++ server/server.js
@@ -1,10 +1,11 @@
 const express = require('express');
+const path = require('path');
+const routes = require('./routes');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('hi');
-});
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(routes);
 
 app.listen(3000);
 
